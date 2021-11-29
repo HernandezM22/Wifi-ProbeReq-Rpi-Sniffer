c418396b7186b7357a454cafcce3cffa276db5f7
Mauricio Hernandez <hernandez.mauricio2220@gmail.com>
2021-11-29 17:06:08 -0600

    added explicit timestamp

--- sniff-probes.sh
+++ sniff-probes.sh
@@ -32,4 +32,4 @@ if [ "$CHANNEL_HOP" -eq 1 ] ; then
 fi
 
 # filter with awk, then use sed to convert tabs to spaces and remove front and back quotes around SSID
-timeout 20s tcpdump -i "wlan1mon" -e -s 256 subtype probe-req | awk -f parse-tcpdump.awk | tee -a "$OUTPUT" 
+timeout 7200s tcpdump -i "wlan1mon" -e -s 256 subtype probe-req | awk -f parse-tcpdump.awk | tee -a "$OUTPUT" 
